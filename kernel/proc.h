6227f651c51794f4393bea20a0e737a6f8402e7d
Mankirat Gujral <145876949+mgujral4@users.noreply.github.com>
2026-04-05 22:38:10 -0400

    .

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct semaphore {
+  int value;
+  struct spinlock lock;
+};
 };
