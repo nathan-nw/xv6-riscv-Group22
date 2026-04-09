ff7e7a487ab6b290d773d3388c89b757c4e200d4
Mankirat Gujral <145876949+mgujral4@users.noreply.github.com>
2026-04-09 03:04:39 -0400

    Update user.h

--- user/user.h
+++ user/user.h
@@ -24,6 +24,10 @@ int getpid(void);
 char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
+int sem_wait(void);
+int sem_signal(void);
+int sem_init(int);
+
 
 // ulib.c
 int stat(const char*, struct stat*);
